a588fd69316c64cd0626f835534aeb21626d7f80
Michael Glen Lorenzo <mikeglenlorenzo.github@gmail.com>
2024-01-04 21:41:37 +0700

    update app.py run for deploy

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    # app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
